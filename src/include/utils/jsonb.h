6e04d486373e00a4a141e946a589cd04b9cc9b5a
Kate <katyagribkova.st@gmail.com>
2026-05-21 18:50:45 +0300

    Extract jsonbFindKeyInObject(), jsonbFindValueInArray(), JsonbArrayIterator

--- src/include/utils/jsonb.h
+++ src/include/utils/jsonb.h
@@ -368,7 +368,7 @@ extern JsonbValue *findJsonbValueFromContainer(const JsonbContainer *container,
 extern JsonbValue *getKeyJsonValueFromContainer(const JsonbContainer *container,
 												const char *keyVal, int keyLen,
 												JsonbValue *res);
-extern JsonbValue *getIthJsonbValueFromContainer(JsonbContainer *container,
+extern JsonbValue *getIthJsonbValueFromContainer(const JsonbContainer *sheader,
 												 uint32 i);
 extern JsonbValue *pushJsonbValue(JsonbParseState **pstate,
 								  JsonbIteratorToken seq, 
