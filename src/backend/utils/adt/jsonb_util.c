ce5e081c9c32c1e94afe817687d4c4ea6b3e334e
Kate <katyagribkova.st@gmail.com>
2026-05-22 12:43:39 +0300

    Rename JsonbIterator to jsonbIterator inside jsonb_util.c

--- src/backend/utils/adt/jsonb_util.c
+++ src/backend/utils/adt/jsonb_util.c
@@ -49,7 +49,7 @@ struct JsonbParseState
 };
 
 
-struct JsonbIterator
+typedef struct jsonbIterator
 {
 	JsonIterator	ji;
 
@@ -78,7 +78,7 @@ struct JsonbIterator
 
 	/* Private state */
 	JsonbIterState state;
-};
+}jsonbIterator;
 
 
 static void fillJsonbValue(const JsonbContainer *container, int index,
@@ -94,7 +94,7 @@ static void convertJsonbScalar(StringInfo buffer, JEntry *header, const JsonbVal
 static void copyToBuffer(StringInfo buffer, int offset, const void *data, int len);
 static short padBufferToInt(StringInfo buffer);
 
-static JsonbIterator *iteratorFromContainer(JsonContainer *container, JsonbIterator *parent);
+static jsonbIterator *iteratorFromContainer(JsonContainer *container, jsonbIterator *parent);
 static JsonbParseState *pushState(JsonbParseState **pstate);
 static void appendKey(JsonbParseState *pstate, const JsonbValue *string);
 static void appendValue(JsonbParseState *pstate, const JsonbValue *scalarVal);
@@ -250,8 +250,8 @@ compareJsonbContainers(JsonContainer *a, JsonContainer *b)
 			   *itb;
 	int			res = 0;
 
-	ita = JsonIteratorInit(a);
-	itb = JsonIteratorInit(b);
+	ita = JsonbIteratorInit(a);
+	itb = JsonbIteratorInit(b);
 
 	do
 	{
@@ -260,8 +260,8 @@ compareJsonbContainers(JsonContainer *a, JsonContainer *b)
 		JsonbIteratorToken ra,
 					rb;
 
-		ra = JsonIteratorNext(&ita, &va, false);
-		rb = JsonIteratorNext(&itb, &vb, false);
+		ra = JsonbIteratorNext(&ita, &va, false);
+		rb = JsonbIteratorNext(&itb, &vb, false);
 
 		if (ra == rb)
 		{
@@ -758,27 +758,27 @@ pushJsonbValue(JsonbParseState **pstate, JsonbIteratorToken seq,
 	}
 
 	/* unpack the binary and add each piece to the pstate */
-	it = JsonIteratorInit(jbval->val.binary.data);
+		it = JsonbIteratorInit(jbval->val.binary.data);
 
 	if (JsonContainerIsScalar(jbval->val.binary.data) && *pstate)
 	{
-		tok = JsonIteratorNext(&it, &v, true);
+		tok = JsonbIteratorNext(&it, &v, true);
 		Assert(tok == WJB_BEGIN_ARRAY);
 		Assert(v.type == jbvArray && v.val.array.rawScalar);
 
-		tok = JsonIteratorNext(&it, &v, true);
+		tok = JsonbIteratorNext(&it, &v, true);
 		Assert(tok == WJB_ELEM);
 
 		res = pushJsonbValueScalar(pstate, seq, &v);
 
-		tok = JsonIteratorNext(&it, &v, true);
+		tok = JsonbIteratorNext(&it, &v, true);
 		Assert(tok == WJB_END_ARRAY);
 		Assert(it == NULL);
 
 		return res;
 	}
 
-	while ((tok = JsonIteratorNext(&it, &v, false)) != WJB_DONE)
+	while ((tok = JsonbIteratorNext(&it, &v, false)) != WJB_DONE)
 		res = pushJsonbValueScalar(pstate, tok,
 								   tok < WJB_BEGIN_ARRAY ||
 								   (tok == WJB_BEGIN_ARRAY &&
@@ -1037,10 +1037,10 @@ appendElement(JsonbParseState *pstate, const JsonbValue *scalarVal)
  * WJB_END_OBJECT.  However, we set val->type = jbvNull in those cases,
  * so that callers may assume that val->type is always well-defined.
  */
-JsonbIteratorToken
-JsonbIteratorNext(JsonIterator **jsit, JsonbValue *val, bool skipNested)
+static JsonbIteratorToken
+jsonbIteratorNext(JsonIterator **jsit, JsonbValue *val, bool skipNested)
 {
-	JsonbIterator **it = (JsonbIterator **) jsit;
+	jsonbIterator **it = (jsonbIterator **) jsit;
 
 	if (*it == NULL)
 	{
@@ -1052,7 +1052,7 @@ JsonbIteratorNext(JsonIterator **jsit, JsonbValue *val, bool skipNested)
 	 * When stepping into a nested container, we jump back here to start
 	 * processing the child. We will not recurse further in one call, because
 	 * processing the child will always begin in JBI_ARRAY_START or
-	 * JBI_OBJECT_START state.
+	 * JBI_OBJECT_START state.JsonbIteratorNext
 	 */
 recurse:
 	switch ((*it)->state)
@@ -1083,7 +1083,7 @@ recurse:
 				 * independently tracks iteration progress at its level of
 				 * nesting).
 				 */
-				*it = (JsonbIterator *)
+				*it = (jsonbIterator *)
 						JsonIteratorFreeAndGetParent((JsonIterator *) *it);
 				val->type = jbvNull;
 				return WJB_END_ARRAY;
@@ -1138,7 +1138,7 @@ recurse:
 				 * (which independently tracks iteration progress at its level
 				 * of nesting).
 				 */
-				*it = (JsonbIterator *)
+				*it = (jsonbIterator *)
 						JsonIteratorFreeAndGetParent((JsonIterator *) *it);
 				
 				val->type = jbvNull;
@@ -1192,10 +1192,10 @@ recurse:
 	return WJB_DONE;
 }
 
-static JsonbIterator *
-iteratorFromContainer(JsonContainer *container, JsonbIterator *parent)
+static jsonbIterator *
+iteratorFromContainer(JsonContainer *container, jsonbIterator *parent)
 {
-	JsonbIterator *it = (JsonbIterator *) JsonIteratorInit(container);
+	jsonbIterator *it = (jsonbIterator *) JsonbIteratorInit(container);
 	it->ji.parent = &parent->ji;
 	return it;
 }
@@ -1207,15 +1207,15 @@ iteratorFromContainer(JsonContainer *container, JsonbIterator *parent)
  * See JsonbIteratorNext() for notes on memory management.
  */
 static JsonIterator *
-JsonbIteratorInit(JsonContainer *cont)
+jsonbIteratorInit(JsonContainer *cont)
 {
 	const JsonbContainer *container = cont->data;
-	JsonbIterator *it;
+	jsonbIterator *it;
 
-	it = palloc0(sizeof(JsonbIterator));
+	it = palloc0(sizeof(jsonbIterator));
 	it->ji.container = cont;
 	it->ji.parent = NULL;
-	it->ji.next = JsonbIteratorNext;
+	it->ji.next = jsonbIteratorNext;
 	it->container = container;
 	it->nElems = container->header & JB_CMASK;
 
@@ -1276,8 +1276,8 @@ JsonbDeepContains(JsonIterator **val, JsonIterator **mContained)
 	 */
 	check_stack_depth();
 
-	rval = JsonIteratorNext(val, &vval, false);
-	rcont = JsonIteratorNext(mContained, &vcontained, false);
+	rval = JsonbIteratorNext(val, &vval, false);
+	rcont = JsonbIteratorNext(mContained, &vcontained, false);
 
 	if (rval != rcont)
 	{
@@ -1312,7 +1312,7 @@ JsonbDeepContains(JsonIterator **val, JsonIterator **mContained)
 			JsonbValue *lhsVal; /* lhsVal is from pair in lhs object */
 			JsonbValue	lhsValBuf;
 
-			rcont = JsonIteratorNext(mContained, &vcontained, false);
+			rcont = JsonbIteratorNext(mContained, &vcontained, false);
 
 			/*
 			 * When we get through caller's rhs "is it contained within?"
@@ -1337,7 +1337,7 @@ JsonbDeepContains(JsonIterator **val, JsonIterator **mContained)
 			 * ...at this stage it is apparent that there is at least a key
 			 * match for this rhs pair.
 			 */
-			rcont = JsonIteratorNext(mContained, &vcontained, true);
+			rcont = JsonbIteratorNext(mContained, &vcontained, true);
 
 			Assert(rcont == WJB_VALUE);
 
@@ -1363,8 +1363,8 @@ JsonbDeepContains(JsonIterator **val, JsonIterator **mContained)
 				Assert(lhsVal->type == jbvBinary);
 				Assert(vcontained.type == jbvBinary);
 
-				nestval = JsonIteratorInit(lhsVal->val.binary.data);
-				nestContained = JsonIteratorInit(vcontained.val.binary.data);
+				nestval = JsonbIteratorInit(lhsVal->val.binary.data);
+				nestContained = JsonbIteratorInit(vcontained.val.binary.data);
 
 				/*
 				 * Match "value" side of rhs datum object's pair recursively.
@@ -1415,7 +1415,7 @@ JsonbDeepContains(JsonIterator **val, JsonIterator **mContained)
 		/* Work through rhs "is it contained within?" array */
 		for (;;)
 		{
-			rcont = JsonIteratorNext(mContained, &vcontained, true);
+			rcont = JsonbIteratorNext(mContained, &vcontained, true);
 
 			/*
 			 * When we get through caller's rhs "is it contained within?"
@@ -1452,7 +1452,7 @@ JsonbDeepContains(JsonIterator **val, JsonIterator **mContained)
 					for (i = 0; i < nLhsElems; i++)
 					{
 						/* Store all lhs elements in temp array */
-						rcont = JsonIteratorNext(val, &vval, true);
+						rcont = JsonbIteratorNext(val, &vval, true);
 						Assert(rcont == WJB_ELEM);
 
 						if (vval.type == jbvBinary)
@@ -1475,8 +1475,8 @@ JsonbDeepContains(JsonIterator **val, JsonIterator **mContained)
 							   *nestContained;
 					bool		contains;
 
-					nestval = JsonIteratorInit(lhsConts[i].val.binary.data);
-					nestContained = JsonIteratorInit(vcontained.val.binary.data);
+					nestval = JsonbIteratorInit(lhsConts[i].val.binary.data);
+					nestContained = JsonbIteratorInit(vcontained.val.binary.data);
 
 					contains = JsonbDeepContains(&nestval, &nestContained);
 
@@ -2257,7 +2257,7 @@ JsonContainerOps
 jsonbContainerOps =
 {
 	jsonbInit,
-	JsonbIteratorInit,
+	jsonbIteratorInit,
 	jsonbFindKeyInObject,
 	jsonbFindValueInArray,
 	jsonbGetArrayElement,
