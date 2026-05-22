75b499b7b7a4bfbaef86e1bf04c7d3cf34216867
Kate <katyagribkova.st@gmail.com>
2026-05-22 14:08:03 +0300

    Add JsonValueUnwrap(), JsonExtractScalar()

--- src/backend/utils/adt/jsonb.c
+++ src/backend/utils/adt/jsonb.c
@@ -772,15 +772,10 @@ datum_to_jsonb_internal(Datum val, bool is_null, JsonbInState *result,
 			case JSONTYPE_JSONB:
 				{
 					Jsonb	   *jsonb = DatumGetJsonbP(val);
-					JsonbIterator *it;
-
-					it = JsonbIteratorInit(&jsonb->root);
 
 					if (JB_ROOT_IS_SCALAR(jsonb))
 					{
-						(void) JsonbIteratorNext(&it, &jb, true);
-						Assert(jb.type == jbvArray);
-						(void) JsonbIteratorNext(&it, &jb, true);
+						JsonExtractScalar(&jsonb->root, &jb);
 						break;
 					}
 
@@ -792,6 +787,7 @@ datum_to_jsonb_internal(Datum val, bool is_null, JsonbInState *result,
 					else
 					{
 						JsonbIteratorToken type;
+						JsonbIterator *it = JsonbIteratorInit(&jsonb->root);
 
 						while ((type = JsonbIteratorNext(&it, &jb, false))
 							   != WJB_DONE)
