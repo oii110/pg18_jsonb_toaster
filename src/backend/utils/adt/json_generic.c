2680feea94b5d4c57251e92b8c0c8cd548d3505a
Kate <katyagribkova.st@gmail.com>
2026-05-24 20:42:21 +0300

    Remove JsonCopyTemporary()

--- src/backend/utils/adt/json_generic.c
+++ src/backend/utils/adt/json_generic.c
@@ -138,17 +138,6 @@ JsonFree(Json *json)
 		pfree(json);
 }
 
-Json *
-JsonCopyTemporary(Json *tmp)
-{
-	Json *json = (Json *) palloc(sizeof(Json));
-
-	memcpy(json, tmp, sizeof(Json));
-	tmp->obj.freeValue = false;
-	json->obj.isTemporary = false;
-
-	return json;
-}
 
 Json *
 JsonValueToJson(JsonValue *val)
