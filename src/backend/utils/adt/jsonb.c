035596efb8e2487cc0d3876e19a2a78cf71b1fb8
Kate <katyagribkova.st@gmail.com>
2026-06-12 15:32:40 +0300

    Minor changes to build jsonb_toaster and make it work

--- src/backend/utils/adt/jsonb.c
+++ src/backend/utils/adt/jsonb.c
@@ -106,10 +106,12 @@ jsonb_recv(PG_FUNCTION_ARGS)
 Datum
 jsonb_out(PG_FUNCTION_ARGS)
 {
+	Jsonb	   *jb;
+	char	   *out;
+
 	jsonbInitIterators();
 
-	Jsonb	   *jb = PG_GETARG_JSONB_P(0);
-	char	   *out;
+	jb = PG_GETARG_JSONB_P(0);
 
 	out = JsonToCString(JsonbRoot(jb), NULL);
 
@@ -127,13 +129,16 @@ jsonb_out(PG_FUNCTION_ARGS)
 Datum
 jsonb_send(PG_FUNCTION_ARGS)
 {
-	jsonbInitIterators();
-
-	Jsonb	   *jb = PG_GETARG_JSONB_P(0);
+		Jsonb	   *jb;
 	StringInfoData buf;
-	StringInfo	jtext = makeStringInfo();
+	StringInfo	jtext;
 	int			version = 1;
 
+	jsonbInitIterators();
+
+	jb = PG_GETARG_JSONB_P(0);
+	jtext = makeStringInfo();
+	
 	(void) JsonToCString(JsonbRoot(jb), jtext);
 	jsonbFreeIterators();
 
